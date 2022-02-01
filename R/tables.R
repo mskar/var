cf0b4ba23b2213672c4002b5227322f7702cd752
Martin Skarzynski <marskar@gmail.com>
2022-02-01 09:28:58 -0500

    add first table

--- R/tables.R
+++ R/tables.R
@@ -1,16 +1,18 @@
 library(kableExtra)
 s1 <- readr::read_tsv("tables/st1.tsv")
 
-s1 |> kbl() |> kable_paper() |> column_spec(
+topbtm <- s1[c(1:5, 35:40),]
+
+topbtm |> kbl() |> kable_paper() |> column_spec(
   6,
   color = "white",
-  background = spec_color(s1$`Odds ratio`, end = .8),
+  background = spec_color(topbtm$`Odds ratio`, end = .8),
   )
-
-
 
 s1 |> tail(8) |> kbl() |> kable_paper() |> column_spec(
   6,
   color = "white",
   background = spec_color(tail(s1$`Odds ratio`, 8), end = .6),
   )
+
+topbtm <- s1[c(1:10, 30:40),]
