5f91a797c787adf3d41aaa45e9bc278a92cc2702
Martin Skarzynski <marskar@gmail.com>
2022-02-02 09:19:22 -0500

    add gisiad logo and data pipeline bullets

--- R/tables.R
+++ R/tables.R
@@ -3,10 +3,15 @@ s1 <- readr::read_tsv("tables/st1.tsv")
 
 topbtm <- s1[c(1:5, 35:40),]
 
-topbtm |> kbl() |> kable_paper() |> column_spec(
-  6,
-  color = "white",
-  background = spec_color(topbtm$`Odds ratio`, end = .8),
+topbtm <- topbtm[order(-topbtm$`Odds ratio`),]
+
+topbtm |>
+  kbl() |>
+  kable_paper() |>
+  column_spec(
+    6,
+    color = "white",
+    background = spec_color(topbtm$`Odds ratio`, end = .8),
   )
 
 s1 |> tail(8) |> kbl() |> kable_paper() |> column_spec(
@@ -15,4 +20,32 @@ s1 |> tail(8) |> kbl() |> kable_paper() |> column_spec(
   background = spec_color(tail(s1$`Odds ratio`, 8), end = .6),
   )
 
-topbtm <- s1[c(1:10, 30:40),]
+
+s2 <- readr::read_tsv("tables/st2.tsv")
+
+s2
+topbtm <- s2[c(1:5, 35:40),]
+
+topbtm |> kbl() |> kable_paper() |> column_spec(
+  5,
+  color = "white",
+  background = spec_color(topbtm$`Odds ratio`, end = .8),
+  )
+
+s3 <- readr::read_tsv("tables/st3.tsv")
+
+s3
+
+s3 |> kbl() |> kable_paper() |> column_spec(
+  2,
+  color = "white",
+  background = spec_color(s3$AUC, end = .6, option = "magma"),
+  ) |>  column_spec(
+  5,
+  color = "white",
+  background = spec_color(s3$`Odds   ratio`, end = .7, option = "viridis"),
+  ) |>  column_spec(
+  3,
+  color = "white",
+  background = spec_color(s3$Accuracy, end = .6, option = "magma"),
+  )
